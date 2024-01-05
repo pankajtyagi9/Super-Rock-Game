ce4483af876434db5ea3d84c1cb16021db914941
pankajtyagi9 <115096536+pankajtyagi9@users.noreply.github.com>
2024-01-05 17:51:17 +0530

    Add files via upload

---
+++ Super-Rocks-Game-main/Super Rocks Game/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  Super Rocks Game
+//
+//  Created by Rajyavardhan Singh.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                            
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+                
+            }
+        }
+        
+    }
+    
+    func deal() {
+        // Randomize the players card
+        let playerCardValue = Int.random(in: 2...14)
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomize the cpus card
+        let cpuCardValue = Int.random(in: 2...14)
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // Update the scores
+        if playerCardValue > cpuCardValue {
+            
+            // Add 1 to player score
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue {
+            
+            // Add 1 to cpu score
+            cpuScore += 1
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Super-Rocks-Game-main/Super Rocks Game/Super_Rocks_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_Rocks_GameApp.swift
+//  Super Rocks Game
+//
+//  Created by Rajyavardhan Singh on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Super_Rocks_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
